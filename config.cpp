3fadf30c0891ed143153284c846b08410bc24540
pkishi <rsdharmaji@gmail.com>
2019-05-24 10:41:50 -0700

    working version

--- config.cpp
+++ config.cpp
@@ -1,15 +1,52 @@
-#include <cstdlib>
-#include <conio.h>
-#include <ctype.h>
-#include <iostream>
+#include "config.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[]){
 
-	if(system("taskkill /F /T /IM ThrottleStop.exe")){
-		cout << "Error: Could not terminate ThrottleStop.\n";
+	char CVC_Input;
+	int E_Flag = -1;
+
+	cout << "Execution of CVC without a valid input device will generate errors.\nExecute CVC? [Y/N]\n";
+
+	while(E_Flag != 0){
+		cout << "\n";
+		cin >> CVC_Input;
+		cout << "\n";
+
+		if(!(CVC_Input == 'y' || CVC_Input == 'n')){
+			cout << "Error: Invalid input.\n";
+			E_Flag = 1;
+		}
+		else{
+			E_Flag =  0;
+		}
 	}
+
+	if(CVC_Input == 'n'){
+		cout << "CVC Disabled.\n";
+	}
+
+	system("taskkill /F /T /IM ThrottleStop.exe");
+
+	if(!ShellExecuteA(NULL, "open", PATH_01, NULL, NULL, SW_HIDE)){
+		cout << "Error: Could not open BNET.\n";
+	}
+
+	if(CVC_Input == 'y'){
+		if(!ShellExecuteA(NULL, "open", PATH_02, NULL, NULL, SW_HIDE)){
+			cout << "Error: Could not open CVC.\n";
+		}
+	}
+
+	if(!ShellExecuteA(NULL, "open", PATH_03, NULL, NULL, SW_HIDE)){
+		cout << "Error: Could not open MSI.\n";
+	}
+	if(!ShellExecuteA(NULL, "open", PATH_04, NULL, NULL, SW_HIDE)){
+		cout << "Error: Could not open TS.\n";
+	}
+
+	cout << "Execution complete. Press any key to exit.\n";
 
 	_getch();
 
---
+++ config.h
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <conio.h>
+#include <ctype.h>
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+#define PATH_01 "C:/files/projects/xps-autoconfig/target/bnet.lnk"
+#define PATH_02 "C:/files/projects/xps-autoconfig/target/cvc.lnk"
+#define PATH_03 "C:/files/projects/xps-autoconfig/target/msi.lnk"
+#define PATH_04 "C:/files/projects/xps-autoconfig/target/ts.lnk"
