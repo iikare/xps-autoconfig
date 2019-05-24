978075a561cd7b6c168a69f0132fffa9dd3475fb
pkishi <rsdharmaji@gmail.com>
2019-05-24 10:29:04 -0700

    add admin manifest

--- config.cpp
+++ config.cpp
@@ -1,6 +1,17 @@
+#include <cstdlib>
+#include <conio.h>
+#include <ctype.h>
+#include <iostream>
 
-
+using namespace std;
 
 int main(int argc, char const *argv[]){
+
+	if(system("taskkill /F /T /IM ThrottleStop.exe")){
+		cout << "Error: Could not terminate ThrottleStop.\n";
+	}
+
+	_getch();
+
 	return 0;
 }
