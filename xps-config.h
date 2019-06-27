71973819bbee24db7885ff8accd8ae677bcdab3a
pkishi <rsdharmaji@gmail.com>
2019-06-27 12:51:32 -0700

    change to relative pathing

--- xps-config.h
+++ xps-config.h
@@ -5,8 +5,8 @@
 #include <windows.h>
 #include <string>
 
-#define PATH_01 "C:/files/projects/xps-autoconfig/target/bnet.lnk"
-#define PATH_02 "C:/files/projects/xps-autoconfig/target/cvc.lnk"
-#define PATH_03 "C:/files/projects/xps-autoconfig/target/msi.lnk"
-#define PATH_04 "C:/files/projects/xps-autoconfig/target/ts.lnk"
-#define PATH_05 "C:/files/projects/xps-autoconfig/target/peace.lnk"
+#define PATH_01 ".\\target\\bnet.lnk"
+#define PATH_02 ".\\target\\cvc.lnk"
+#define PATH_03 ".\\target\\msi.lnk"
+#define PATH_04 ".\\target\\ts.lnk"
+#define PATH_05 ".\\target\\peace.lnk"
